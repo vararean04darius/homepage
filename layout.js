a7d8f436c2f0d67fec05039e4d8960d069b3bbe7
vararean04darius <91600164+vararean04darius@users.noreply.github.com>
2025-01-09 13:16:08 +0200

    final touches

--- layout.js
+++ layout.js
@@ -4,9 +4,14 @@ const myGrid = document.getElementById("grid");
 
 let myProjects = ["admin", "battleship", "calculator", "pizzeria", "signup", "weather"];
 let myPictures = ["./admin.png", "./battleship.png", "./calculator.png", "./pizzeria.png", "./signup.png", "./weather.png"];
-let descriptons =
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ullam, molestias, unde quis incidunt quam culpa officia qui.";
-
+// "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ullam, molestias, unde quis incidunt quam culpa officia qui.";
+let adminDescription = "This admin panel is a simple project that I created while learning html, js, and especially css.";
+let battleshipDescription = "This is a game that I created while learning js and TDD (Test Driven Development). It is only playable locally, single/multiplayer.";
+let calculatorDescription = "The calculator was my first 'bigger' project. It taught me a lot about js and also gave me some headaches while trying to make it work. Did you know that 0.1 + 0.2 it's actually 0.3 in JS?";
+let pizzeriaDescription = "This pizzeria website was a nice project that helped me think about html as a powerful tool for the css. Also made me understand the giant impact of JavaScript in creating layouts.";
+let signupDescription = "The signup form was a project that I created to learn about the importance of validation in forms. It was a nice project that helped me understand the importance of the user experience.";
+let weatherDescription = "I spent quite some time doing the weather app project since it taught me how to use API calls and how to extract information from outside the project.";
+let descriptons = [adminDescription, battleshipDescription, calculatorDescription, pizzeriaDescription, signupDescription, weatherDescription];
 for (let i = 0; i < 6; i++) {
   const square = document.createElement("div");
   square.classList.add("card");
@@ -56,7 +61,7 @@ for (let i = 0; i < 6; i++) {
 
   const descriptionMessage = document.createElement("p");
   descriptionMessage.classList.add("description-message");
-  descriptionMessage.textContent = descriptons;
+  descriptionMessage.textContent = descriptons[i];
 
   textDescriptionContainer.appendChild(descriptionMessage);
 }
